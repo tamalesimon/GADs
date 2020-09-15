c200e01a2f620e66fbc435856bfc6ae876d87ff6
tamalesimon <tamalesp@gmail.com>
2020-09-15 04:03:08 +0300

    country code and cleaning up css

--- my-weather/app.js
+++ my-weather/app.js
@@ -30,14 +30,39 @@ window.addEventListener("load", () => {
                 
                 temperatureDescription.textContent = weather[0].description;
                 console.log(data.sys); 
+
+                placeCode.textContent = sys.country;
             
 
-                let Icon = document.getElementById('icon-info').src = `https://openweathermap.org/img/wn/${weather[0].icon}@2x.png`;
+                let Icon = document.getElementById('icon-info').src = `https://openweathermap.org/img/wn/${weather[0]["icon"]
+            }@2x.png`;
                 
                 let current_datetime = new Date();
-                let formatted_date = current_datetime.getFullYear() + "-" + (current_datetime.getMonth() + 1) + "-" + current_datetime.getDate() + " " + current_datetime.getHours() + ":" + current_datetime.getMinutes() + ":" + current_datetime.getSeconds();
-                
+                var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+                var month=new Array();
+                month[0]="Jan";
+                month[1]="Feb";
+                month[2]="Mar";
+                month[3]="Apr";
+                month[4]="May";
+                month[5]="Jun";
+                month[6]="Jul";
+                month[7]="Aug";
+                month[8]="Sep";
+                month[9]="Oct";
+                month[10]="Nov";
+                month[11]="Dec";
+                var hours = current_datetime.getHours();
+                var minutes = current_datetime.getMinutes();
+                var ampm = hours >= 12 ? 'pm' : 'am';
+                hours = hours % 12;
+                hours = hours ? hours : 12;
+                minutes = minutes < 10 ? '0'+minutes : minutes;
+                var strTime = hours + ':' + minutes + ampm;
 
+                let formatted_date = strTime+" "+"-"+" "+ days[current_datetime.getDay()]+","+" "+current_datetime.getDate()+ " "+month[current_datetime.getMonth()]+" "+current_datetime.getFullYear();
+                
+                //current_datetime.getFullYear() + "-" + (current_datetime.getMonth() + 1) + "-" + current_datetime.getDate() + " " + current_datetime.getHours() + ":" + current_datetime.getMinutes() + ":" + current_datetime.getSeconds()
                 DateTime.textContent = formatted_date;
 
             });
